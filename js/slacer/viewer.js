0c91d7283b7a9344d7f66b4b2dc182c8f8852961
Kareem Shaik <kareem@magnitude.io>
2020-01-08 15:05:39 -0800

    Revert "Update viewer.js"
    This reverts commit fb9d1cd17dc0683fcbb9a05b4da4cf4f03d28cfa.

--- js/slacer/viewer.js
+++ js/slacer/viewer.js
@@ -34,7 +34,7 @@ var SLAcer = SLAcer || {};
 
         // create main objects
         self.scene    = new THREE.Scene();
-        self.camera   = new THREE.OrthographicCamera(-600,600,400,-400,1,1000);
+        self.camera   = new THREE.OrthographicCamera(-300,300,200,-200,1,1000);
         self.renderer = new THREE.WebGLRenderer({ antialias: self.settings.antialias });
 
         // assign camera settings
